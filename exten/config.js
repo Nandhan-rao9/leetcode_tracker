5a721d3b812bf1f602883d64db8e88bc3c8418f2
Nandhan <nandhanrao99@example.com>
2025-08-12 21:51:14 +0530

    cleaning

--- exten/config.js
+++
@@ -1,5 +0,0 @@
-// TODO: set these for your machine
-export const BACKEND_URL = "http://127.0.0.1:8080";
-export const SHARED_SECRET = "my-key"; // same as EXTENSION_SHARED_SECRET in your API .env
-export const USER_ID = "harshan";        // temporary until we add auth; per your backend
-
